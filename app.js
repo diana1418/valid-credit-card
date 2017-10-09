26ecadd2151e7a3e82bb092be2270b00dcaedc6a
diana1418 <a20166303@pucp.edu.pe>
2017-10-09 12:40:34 -0500

    valid credit card 1

---
+++ app.js
@@ -0,0 +1,9 @@
+function isValidCard(digits){
+  var creditCard=parseInt(prompt('Ingrese su número de tarjeta'));
+for(var i=0;i<digits;i++){
+  if(creditCard.charAt(i)!==' ' && creditCard.length===16){
+    
+
+   }
+  }
+}
